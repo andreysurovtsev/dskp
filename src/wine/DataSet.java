b6bfdfca7139b18f6ce78d6a201c50d908ba13d6
Andrey Surovtsev <andrey.v.surovtsev@gmail.com>
2018-04-11 11:05:16 +0300

    fixed wine.txt file (removed an empty line in the end), refactored file reading routines.

--- src/wine/DataSet.java
+++ src/wine/DataSet.java
@@ -9,44 +9,70 @@ public final class DataSet {
 
 	private DataSample[] dataArray;
 
-	public DataSet(String filename) throws FileNotFoundException {
-		File file = new File(filename);
-		int i = 0;
-		int label_s = 0;
-		this.dataArray = new DataSample[this.getDataSetSize(filename)];
+	public DataSet(String filename) {
+
+		try {
+			this.dataArray = new DataSample[getDataSetSize(filename)];
+			readDataSet(filename);
+		} catch (FileNotFoundException e) {
+			System.out.println("Error occured when loading a file " + filename);
+			System.out.println(e);
+		}
+
+		/*
+		 * while (input.hasNextLine()) { String[] sampleArr =
+		 * ((input.next()).split(",")); double[] attr = new
+		 * double[sampleArr.length - 1]; label_s =
+		 * Integer.parseInt(sampleArr[0]);
+		 * 
+		 * for (int j = 0; j < (sampleArr.length - 1); j++) { attr[j] =
+		 * Double.parseDouble(sampleArr[j + 1]); } DataSample data = new
+		 * DataSample(label_s, attr);
+		 * 
+		 * dataArray[i] = data; i++; } String taskOutputString =
+		 * "Mean of class 1: " + Arrays.toString(this.getMean(1)) + "\n" +
+		 * "Std of class 1: " + Arrays.toString(this.getStd(1)) + "\n" +
+		 * "Mean of class 2: " + Arrays.toString(this.getMean(2)) + "\n" +
+		 * "Std of class 2: " + Arrays.toString(this.getStd(2)) + "\n" +
+		 * "Mean of class 3: " + Arrays.toString(this.getMean(3)) + "\n" +
+		 * "Std of class 3: " + Arrays.toString(this.getStd(3)) + "\n";
+		 */
+	}
+
+	private void readDataSet(String fileName) throws FileNotFoundException {
+		File file = new File(fileName);
 
 		Scanner input = new Scanner(file);
+
+		int i = 0;
 
 		while (input.hasNextLine()) {
 			String[] sampleArr = ((input.next()).split(","));
 			double[] attr = new double[sampleArr.length - 1];
-			label_s = Integer.parseInt(sampleArr[0]);
+
+			int label = Integer.parseInt(sampleArr[0]);
 
 			for (int j = 0; j < (sampleArr.length - 1); j++) {
 				attr[j] = Double.parseDouble(sampleArr[j + 1]);
 			}
-			DataSample data = new DataSample(label_s, attr);
 
-			dataArray[i] = data;
-			i++;
+			dataArray[i++] = new DataSample(label, attr);
 		}
-		String taskOutputString = "Mean of class 1: " + Arrays.toString(this.getMean(1)) + "\n" + "Std of class 1: "
-				+ Arrays.toString(this.getStd(1)) + "\n" + "Mean of class 2: " + Arrays.toString(this.getMean(2)) + "\n"
-				+ "Std of class 2: " + Arrays.toString(this.getStd(2)) + "\n" + "Mean of class 3: "
-				+ Arrays.toString(this.getMean(3)) + "\n" + "Std of class 3: " + Arrays.toString(this.getStd(3)) + "\n";
+		input.close();
+
 	}
 
-	public int getDataSetSize(String filename) throws FileNotFoundException {
+	private int getDataSetSize(String filename) throws FileNotFoundException {
 		int samples = 0;
 		File file = new File(filename);
 
-		try (Scanner input = new Scanner(file)) {
-			while (input.hasNextLine()) {
-				input.nextLine();
-				samples++;
-			}
-			input.close();
+		Scanner input = new Scanner(file);
+		while (input.hasNextLine()) {
+			input.nextLine();
+			samples++;
 		}
+		input.close();
+
 		return samples;
 	}
 
--- src/wine/main.java
+++ src/wine/main.java
@@ -6,6 +6,6 @@ import java.io.FileNotFoundException;
 public class main {
       public static void main(String[] args) throws FileNotFoundException {
          DataSet train = new DataSet("wine.txt");
-         System.out.println(train.taskOutputString);
+         // System.out.println(train.taskOutputString);
     }
 }
